0a7dd01509f8b7c50d8d6bdd301c1002b2974584
julia <juliatong2014thl@foxmail.com>
2021-02-24 16:14:17 +0800

    add readme

--- graph/src/test/java/com/example/graph/client/ExtremCaseTests.java
+++ graph/src/test/java/com/example/graph/client/ExtremCaseTests.java
@@ -7,4 +7,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ExtremCaseTests {
+
+    //1. when it draws, it is out of canvas
+
+    //2. when it draws, it hits the landmine
+
+    //3.error in input args
+
+    //4
 }
