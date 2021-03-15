d5520278dab44be2f7a5945d0e58198231cb4fdd
julia <juliatong2014thl@foxmail.com>
2021-03-15 19:19:21 +0800

    unique BST:Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. Return the answer in any order.

--- graph/src/test/java/UniqueBST.java
+++ graph/src/test/java/UniqueBST.java
@@ -27,40 +27,67 @@ public class UniqueBST {
 
     public static void main(String args[]){
         List<Integer> integers = Lists.newArrayList(1, 2, 3);
-        ArrayList<TreeNode> roots = new ArrayList<>();
-        constructTree(integers,roots);
+        constructTree(integers);
     }
 
-    public static TreeNode constructTree( List<Integer> list,List<TreeNode> roots ) {
-        TreeNode parentNode = null;
+    public static List<TreeNode> constructTree( List<Integer> list ) {
+        List<TreeNode> roots = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
-            parentNode = new TreeNode(list.get(i));
+            TreeNode parentNode = new TreeNode(list.get(i));
             roots.add(parentNode);
 
             if (list.size() == 1) {
-                return parentNode;
+                roots.add(parentNode);
+                return roots;
             }
 
             int tail = list.size();
+            List<TreeNode> leftNodes = new ArrayList<>();
+            List<TreeNode> rightNodes = new ArrayList<>();
             //left
             if (i != 0) {
-                TreeNode leftNode = constructTree(list.subList(0, i), roots);
-                parentNode.left = leftNode;
+                leftNodes.addAll(constructTree(list.subList(0, i)));
+//                parentNode.left = leftNode;
             } else {
                 parentNode.left = null;
             }
 
             //right
             if (i != tail) {
-                TreeNode rightNode = constructTree(list.subList(i + 1, tail), roots);
-                parentNode.right = rightNode;
+                rightNodes.addAll(constructTree(list.subList(i + 1, tail)));
+//                parentNode.right = rightNode;
             } else {
                 parentNode.right = null;
             }
 
+            for (TreeNode node : leftNodes) {
+                for (TreeNode node2 : rightNodes) {
+                    parentNode.left = node;
+                    parentNode.right = node2;
+                    roots.add(parentNode);
+                }
+            }
         }
-        return parentNode;
+        return roots;
     }
 
-
+    private List<TreeNode> genTreeList (int start, int end) {
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        if (start > end) {
+            list.add(null);
+        }
+        for(int idx = start; idx <= end; idx++) {
+            List<TreeNode> leftList = genTreeList(start, idx - 1);
+            List<TreeNode> rightList = genTreeList(idx + 1, end);
+            for (TreeNode left : leftList) {
+                for(TreeNode right: rightList) {
+                    TreeNode root = new TreeNode(idx);
+                    root.left = left;
+                    root.right = right;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
+    }
 }
